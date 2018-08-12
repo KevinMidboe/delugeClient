50fe8adaf1402bb349e9e8950400ba4784381fc3
Kevin Midbøe <kevin.midboe@gmail.com>
2018-08-12 19:22:32 +0200

    Imported deluge and on connect send currently downloading items.

--- deluge_socket.py
+++ deluge_socket.py
@@ -3,6 +3,8 @@ import datetime
 import random
 import websockets
 
+import deluge_cli
+
 async def hello(websocket, path):
 	name = await websocket.recv()
 	print(f"< {name}")
@@ -19,9 +21,14 @@ async def time(websocket, path):
 		await asyncio.sleep(1)
 
 
+async def deluge(websocket, path):
+	downloading = deluge.main(['ls', '--downloading'])
+	await websocket.send()
+
 serve_hello = websockets.serve(hello, '0.0.0.0', 8765)
-serve_time = websockets.serve(time, '0.0.0.0', 5678)
+# serve_time = websockets.serve(time, '0.0.0.0', 5678)
+serve_deluge = websockets.serve(deluge, '0.0.0.0', 5678)
 
 asyncio.get_event_loop().run_until_complete(serve_hello)
-asyncio.get_event_loop().run_until_complete(serve_time)
+asyncio.get_event_loop().run_until_complete(serve_deluge)
 asyncio.get_event_loop().run_forever()
