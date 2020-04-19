cfe14dd1ba4c5635e44a283ea426d2d776e09cca
KevinMidboe <kevin.midboe@gmail.com>
2020-04-19 14:23:45 +0200

    Changed formatting encoding from ascii to utf-8

--- utils.py
+++ utils.py
@@ -35,7 +35,7 @@ class ColorizeFilter(logging.Filter):
       return True
 
 def convert(data):
-    if isinstance(data, bytes):  return data.decode('ascii')
+    if isinstance(data, bytes):  return data.decode('utf-8')
     if isinstance(data, dict):   return dict(map(convert, data.items()))
     if isinstance(data, tuple):  return map(convert, data)
     json_data = json.dumps(data)
