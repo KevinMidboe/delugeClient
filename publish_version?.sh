103696e01ab6cf4401ce088b7faa02772a0c1ea0
Kevin Midboe <kevin.midboe@gmail.com>
2022-11-25 01:03:59 +0100

    Clearer output text for publish version

--- publish_version?.sh
+++ publish_version?.sh
@@ -3,18 +3,17 @@
 PYPI_VERSION=$(pip3 show delugeClient-kevin | awk '$1 ~ /Version:/ { print $2 }')
 SOURCE_VERSION=$(python3 delugeClient/__version__.py)
 
-echo "hello"
-echo "pypi version: $PYPI_VERSION"
-echo "source version: $SOURCE_VERSION"
+printf "Source version:\t\t %s\n" $SOURCE_VERSION
+printf "Remote PyPi version:\t %s\n" $PYPI_VERSION
 
 function version {
   echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
 }
 
 if [ $(version $SOURCE_VERSION) -gt $(version $PYPI_VERSION) ]; then
-  echo "source is newer than pypi"
+  echo "Soure is newer than remote, publishing!"
   exit 0
 else
+  echo "Source is same or oldre than remote, nothing to do."
   exit 1
-  echo "source is same or oldre, but not newer"
 fi
